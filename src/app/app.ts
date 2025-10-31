26758887f15846d43db931b7b99b6d9a46b21476
rahulder <rahulder08176@gmail.com>
2025-10-31 17:23:02 +0530

    Create mdForms two field first and last name

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Model-driven-form');
+
+  mdForm!: FormGroup;
+  // fb: FormBuilder = Inject(FormBuilder);
+
+  constructor(private fb: FormBuilder) {
+    this.mdForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength]],
+      lastname: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {}
+
+  get firstname() {
+    return this.mdForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.mdForm.get('lastname');
+  }
 }
